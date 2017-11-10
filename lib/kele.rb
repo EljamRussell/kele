0fe9ece8350032f4e4f7819fa52b08539d43964b
Eljam Russell <eljamrussell@gmail.com>
2017-11-10 08:52:45 -0500

    messaging completed

--- lib/kele.rb
+++ lib/kele.rb
@@ -25,6 +25,27 @@ class Kele
     @mentor_availability = JSON.parse(response.body)
   end
 
+  def get_messages(page_num=nil)
+    if page_num == nil
+      response = self.class.get(api_url("/message_threads"), headers: { "authorization" => @auth_token })
+    else
+      response = self.class.get(api_url("/message_threads?page=#{page_num}"), headers: { "authorization" => @auth_token })
+    end
+    @messages = JSON.parse(response.body)
+  end
+
+  def create_message(sender, recipient_id, token, subject, stripped_text)
+    response = self.class.post(api_url("/messages"), body: {
+        "sender": sender,
+        "recipient_id": recipient_id,
+        "token": token,
+        "subject": subject,
+        "stripped_text": stripped_text
+        },
+        headers: {"authorization" => @auth_token})
+    #puts response
+  end
+
   private
 
   def api_url(endpoint)
--- lib/roadmap.rb
+++ lib/roadmap.rb
@@ -1,13 +1,15 @@
 module Roadmap
 
+  # use roadmap_id 31 for testing
   def get_roadmap(roadmap_id)
     response = self.class.get(api_url("/roadmaps/#{roadmap_id}"), headers: { "authorization" => @auth_token })
     @roadmap_data = JSON.parse(response.body)
   end
 
+  #use checkpoint_id 1621 for testing
   def get_checkpoint(checkpoint_id)
     response = self.class.get(api_url("/checkpoints/#{checkpoint_id}"), headers: { "authorization" => @auth_token })
     @checkpoint_id = JSON.parse(response.body)
   end
-  
+
 end
